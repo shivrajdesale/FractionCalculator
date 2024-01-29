714c48605f2f90a017fa722ebe80745f2a425a8d
shivrajdesale <shivrajdesale@gmal.com>
2024-01-29 23:14:41 +0530

    Initial Project Setup

---
+++ src/test/java/com/shiv/calculator/ProgramTest.java
@@ -0,0 +1,16 @@
+package com.shiv.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void launch() {
+    }
+
+    @Test
+    void exit() {
+    }
+}
